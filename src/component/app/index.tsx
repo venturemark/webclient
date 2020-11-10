883dd99f8110e20299d814f26abb1e2265e163ae
Marcus Ellison <marcus.ellison@gmail.com>
2020-11-10 12:37:48 -0800

    Plasmic2 sync view structure action metric button animation (#14)
    * update structure; import statements, prettier, project name
    
    * react easy flip poc
    
    * flip card poc
    
    * after prettier
    
    * remove warning with icon button
    
    * prettify iconButton
    
    Co-authored-by: Marcus Ellison <marcus@venturemark.co>

--- src/component/app/index.tsx
+++ src/component/app/index.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-import logo from "asset/logo.svg";
-
 import "./index.css";
 import * as update from "component/update";
 
--- src/component/linechart/index.tsx
+++ src/component/linechart/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { AreaChart, Area, Tooltip, XAxis } from 'recharts' 
+import React from "react";
+import { AreaChart, Area, Tooltip, XAxis } from "recharts";
 
 type DataItem = {
-  [key:string]: string | number
-}
+  [key: string]: string | number;
+};
 
 interface ChartProps {
-  dataKey: string,
-  data: DataItem[],
+  dataKey: string;
+  data: DataItem[];
 }
 
-const LineChart: React.FC<ChartProps> = ({ data, dataKey }) => {
+export const Component: React.FC<ChartProps> = ({ data, dataKey }) => {
   return (
     <AreaChart
       width={640}
@@ -20,22 +20,20 @@ const LineChart: React.FC<ChartProps> = ({ data, dataKey }) => {
     >
       <defs>
         <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-          <stop offset="8.82%" stopColor={'#029D7F'} stopOpacity={0.15} />
-          <stop offset="79.41%" stopColor={'#029D7F'} stopOpacity={0} />
+          <stop offset="8.82%" stopColor={"#029D7F"} stopOpacity={0.15} />
+          <stop offset="79.41%" stopColor={"#029D7F"} stopOpacity={0} />
         </linearGradient>
       </defs>
       <Tooltip />
       <XAxis tick={false} hide={true} dataKey="date" />
       <Area
         type="monotone"
-        dot={{ strokeWidth: 1, fill: '#FFFFFF' }}
+        dot={{ strokeWidth: 1, fill: "#FFFFFF" }}
         dataKey={dataKey}
-        stroke={'#029D7F'}
+        stroke={"#029D7F"}
         fillOpacity={1}
         fill="url(#colorUv)"
       />
     </AreaChart>
-  )
-}
-
-export default LineChart
+  );
+};
