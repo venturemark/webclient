aee19006709a669ac9a6e5b63939930e1c5e5257
Marcus Ellison <marcus@venturemark.co>
2021-01-30 15:46:25 -0800

    remove metric stuff

--- src/module/api/index.ts
+++ src/module/api/index.ts
@@ -1,13 +1,11 @@
 import * as update from './update';
 import * as timeline from './timeline';
 import * as texUpd from './texupd';
-import * as metric from './metric';
 import * as audience from './audience';
 
 export const API = {
   Audience: audience.Audience,
   Update: update.Update,
   Timeline: timeline.Timeline,
-  Metric: metric.Metric,
   TexUpd: texUpd.TexUpd,
 };
--- src/module/api/metricupdate/index.ts
+++
@@ -1,5 +0,0 @@
-import * as create from './create';
-
-export const MetricUpdate = {
-  Create: create.Create,
-};
