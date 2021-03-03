95a10b5db3179731d7cb099a7200d21d580f1fbe
Marcus Ellison <marcus@venturemark.co>
2021-03-02 16:29:49 -0800

    update isDev

--- src/module/helpers/index.ts
+++ src/module/helpers/index.ts
@@ -1,8 +1,7 @@
 import process from "process";
 
-const development: boolean =
-  !process.env.NODE_ENV || process.env.NODE_ENV === "development";
-
 export function isDev(): boolean {
+  const development: boolean =
+    !process.env.NODE_ENV || process.env.NODE_ENV === "development";
   return development;
 }
