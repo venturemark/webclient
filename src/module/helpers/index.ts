4c8fed661d6e020025d6fafb37f614734721c2a9
Marcus Ellison <marcus@venturemark.co>
2021-03-02 16:33:37 -0800

    ensure node_env = development in boolean

--- src/module/helpers/index.ts
+++ src/module/helpers/index.ts
@@ -1,7 +1,6 @@
 import process from "process";
 
 export function isDev(): boolean {
-  const development: boolean =
-    !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+  const development: boolean = process.env.NODE_ENV === "development";
   return development;
 }
