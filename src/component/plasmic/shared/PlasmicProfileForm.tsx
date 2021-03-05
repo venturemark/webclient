43c9a8acbe69180f0c6fba60fd49d21609d54951
Marcus Ellison <marcus@venturemark.co>
2021-03-04 16:28:51 -0800

    pretty

--- src/component/plasmic/shared/PlasmicProfileForm.tsx
+++ src/component/plasmic/shared/PlasmicProfileForm.tsx
@@ -374,7 +374,8 @@ type NodeOverridesType<T extends NodeNameType> = Pick<
   PlasmicProfileForm__OverridesType,
   DescendantsType<T>
 >;
-type NodeComponentProps<T extends NodeNameType> = { // Explicitly specify variants, args, and overrides as objects
+type NodeComponentProps<T extends NodeNameType> = {
+  // Explicitly specify variants, args, and overrides as objects
   variants?: PlasmicProfileForm__VariantsArgs;
   args?: PlasmicProfileForm__ArgsType;
   overrides?: NodeOverridesType<T>;
