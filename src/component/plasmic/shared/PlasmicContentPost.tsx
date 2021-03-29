16ff4efa4e857862b48404e53276c401ba02cdca
Marcus Ellison <marcus@venturemark.co>
2021-03-29 16:57:24 -0400

    prettier

--- src/component/plasmic/shared/PlasmicContentPost.tsx
+++ src/component/plasmic/shared/PlasmicContentPost.tsx
@@ -530,7 +530,8 @@ type NodeOverridesType<T extends NodeNameType> = Pick<
   PlasmicContentPost__OverridesType,
   DescendantsType<T>
 >;
-type NodeComponentProps<T extends NodeNameType> = { // Explicitly specify variants, args, and overrides as objects
+type NodeComponentProps<T extends NodeNameType> = {
+  // Explicitly specify variants, args, and overrides as objects
   variants?: PlasmicContentPost__VariantsArgs;
   args?: PlasmicContentPost__ArgsType;
   overrides?: NodeOverridesType<T>;
--- src/component/plasmic/shared/PlasmicJoinVenture.tsx
+++ src/component/plasmic/shared/PlasmicJoinVenture.tsx
@@ -223,7 +223,8 @@ type NodeOverridesType<T extends NodeNameType> = Pick<
   PlasmicJoinVenture__OverridesType,
   DescendantsType<T>
 >;
-type NodeComponentProps<T extends NodeNameType> = { // Explicitly specify variants, args, and overrides as objects
+type NodeComponentProps<T extends NodeNameType> = {
+  // Explicitly specify variants, args, and overrides as objects
   variants?: PlasmicJoinVenture__VariantsArgs;
   args?: PlasmicJoinVenture__ArgsType;
   overrides?: NodeOverridesType<T>;
