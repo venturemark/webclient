c32fd2b16e666adcb5a4fe3cc7d0f5545fa3aa91
Marcus Ellison <marcus@venturemark.co>
2021-05-06 18:51:08 -0700

    add timeline name max length error

--- src/module/errors/index.ts
+++ src/module/errors/index.ts
@@ -1,6 +1,7 @@
 export const nameError = "Please enter your name";
 export const timelineNameError = "Please enter a timeline name";
 export const ventureNameError = "Please enter a venture name";
+export const nameTooLongError = "Please enter a shorter name";
 export const titleError = "Please tell us what you do :-)";
 export const emailError = "please enter an email";
 export const descriptionError = "please enter a description";
